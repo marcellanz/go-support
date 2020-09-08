7e0e7254a13c36564ab50498434ff1ecc5b66740
Marcel Lanz <marcellanz@n-1.ch>
2020-09-08 22:51:27 +0200

    [devcontainer] remoded DOCKER_BUILDKIT as it is not supported by travis-ci.

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 readonly CMD=${1:-image}
 readonly IMAGE=cloudstateio/cloudstate-go-devcontainer:latest
-DOCKER_BUILDKIT=1 docker build . -t "$IMAGE"
+docker build . -t "$IMAGE"
 if [ "$CMD" == "push" ]; then
   docker push "$IMAGE"
 fi
